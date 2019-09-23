456e40cc2ceda0544b97020219f9d87558bac1dc
unknown <akxltmzk@gmail.com>
2019-09-23 19:09:12 +0900

    navbar body expend but will fix it with out bootstrap

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,42 @@ $(function () {
 
   // ------------ intropage ----------------//
 
+  // navbar
+
+  let bodyexpend = false; // toggle down
+
+  $('#introdropdown').on("click",()=>{
+    if(!bodyexpend){
+      $('body').animate({
+        "margin-top" : "+=128px" 
+      })
+      bodyexpend = true
+    }
+    else{
+      $('body').animate({
+        "margin-top" : "-=128px"
+      }) 
+      bodyexpend = false
+    }
+  })
+
   // intro-image-section
-  
+
+  $('.content.intropage .slider .item').click(function()
+  {
+
+    let windowWith = screen.availWidth
+    let mousePosition_x = event.pageX
+
+    if((windowWith/2 < mousePosition_x)){
+      $('html').animate( { scrollLeft: '+=460' }, 700)
+    }
+    else{
+      $('html').animate( { scrollLeft: '-=460' }, 700)
+    }
+  })
+
+   
+
   // ------------ intropage end ----------------//
 })

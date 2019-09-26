dfd59ea9ec5729d51276163f133c09893c68ffd9
unknown <akxltmzk@gmail.com>
2019-09-26 16:23:08 +0900

    manager page start

--- routes/manager.js
+++ routes/manager.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Page = require('../models/page')
 
+
+router.get('/manager', function(req, res) {
+
+  // send intro image
+  Page.find({},(err,page)=>{
+    res.render('manager',{page:page})     
+  })
+})
 
 module.exports = router;
